c27bdadda699ca742825a5a32fcee8b324fc8a52
Corinna <cgries@wisc.edu>
2024-10-09 15:14:02 -0500

    Update reformat_kw.R

--- R/reformat_kw.R
+++ R/reformat_kw.R
@@ -1,5 +1,8 @@
+# this script will format the new keywords into columns of their main category
+# and output them for each LTER site
 
 library(tidyverse)
+library(googledrive)
 
 output_path <- "assigned_kw/new_keywords"
 
@@ -41,45 +44,5 @@ for (j in 1:nrow(df_scope)) {
     filter(scope == df_scope[[1]][j])
   
   write.csv(df_ds_site, file = paste(output_path, '/', df_scope[[1]][j], '_new_keywords.csv', sep = ''), row.names = F)
-}
-
-# no keywords
-
-# Identify cases where keywords are present
-df_eco_pres <- distinct(df_ds_out, packageid)
-
-# Remove them via `anti_join`
-df_eco_abs <- anti_join(df_datasets, df_eco_pres, by = c('packageid'))
-
-# 
-df_eco_abs <- separate(df_eco_abs, packageid, into = c('scope', 'dataset_id'), sep = '\\.', remove = F)
-df_scope <- distinct(df_eco_abs, scope)
-
-# Export the set of files that lack ecosystem information
-# write.csv(df_eco_abs, file = file.path(output_path, 'no_ecosystem.csv'), row.names = F)
-
-
-for (j in 1:nrow(df_scope)) {
-  
-  df_ds_site <- df_eco_abs %>%
-    filter(scope == df_scope[[1]][j])
-  
-  write.csv(df_ds_site, file = paste(output_path, '/', df_scope[[1]][j], '_no_keyword.csv', sep = ''), row.names = F)
-}
-
-# ----------------------------------
-# write all files to google drive
-
-upload_url <- googledrive::as_id("https://drive.google.com/drive/folders/1lhvcgktZV2QIYEBdtDvL4mElxf2WZZTc")
-
-file_names <- dir(output_path)
-
-for (j in 1:length(file_names)) {
-  
-  drive_upload(file.path(output_path, file_names[j]),
-               path = upload_url,
-               type = "spreadsheet",
-               overwrite = TRUE)
-
 }
 
